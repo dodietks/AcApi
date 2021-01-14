b77c8ad5da7e13c7b4283867d39fc6b16e7c1e14
Thomas kimberly <thomasklfs@gmail.com>
2021-01-14 18:07:19 -0300

    first commit

---
+++ AcApi/Models/CardRead.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AcApi.Models
+{
+    public class CardRead
+    {
+        [JsonProperty("id")]
+        [Required]
+        public string id { get; set; }
+
+        [JsonProperty("numeroCartao")]
+        [Required]
+        public string cardNumber { get; set; }
+
+        [JsonProperty("tipoPrincipal")]
+        [Required]
+        public string majorType { get; set; }
+
+        [JsonProperty("tipoSecundario")]
+        [Required]
+        public string minorType { get; set; }
+
+        [JsonProperty("data")]
+        [Required]
+        public string dateTime { get; set; }
+    }
+}
---
+++ AcApi/Models/Login.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AcApi.Models
+{
+    public class Login
+    {
+        [JsonProperty("ip")]
+        [Required]
+        public string ip { get; set; }
+
+        [JsonProperty("nome")]
+        [Required]
+        public string name { get; set; }
+
+        [JsonProperty("senha")]
+        [Required]
+        public string password { get; set; }
+
+        [JsonProperty("porta")]
+        [Required]
+        public string port { get; set; }
+
+        [JsonProperty("funcao")]
+        [Required]
+        public int function { get; set; }
+    }
+}
